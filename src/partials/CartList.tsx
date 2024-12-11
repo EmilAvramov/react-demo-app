f69f51b03af653d88a5eb0ab73b6fd4466040608
EmilAvramov <emil.avr@appgr8.com>
2024-12-11 20:51:06 +0200

    style adjustments

--- src/partials/CartList.tsx
+++ src/partials/CartList.tsx
@@ -15,25 +15,30 @@ export const CartList: React.FC<ICartList> = ({ lineItems }) => {
 		<div className='cart-li-container'>
 			{lineItems?.map((li, index) => {
 				return (
-					<div key={index}>
-						<p>{li.name}</p>
-						<p>{li.price * li.quantity}</p>
-						<RemoveIcon
-							onClick={() => {
-								cartContext?.removeItemFromCart(li.id);
-							}}
-						/>
-						<p>{li.quantity}</p>
-						<AddIcon
-							onClick={() => {
-								cartContext?.addItemToCart({
-									id: li.id,
-									name: li.name,
-									price: li.price,
-									quantity: 1,
-								});
-							}}
-						/>
+					<div
+						className='cart-li-item'
+						key={index}
+					>
+						<p>Book Title: {li.name}</p>
+						<p>Total price: {li.price * li.quantity}</p>
+						<div className='cart-li-quantity'>
+							<RemoveIcon
+								onClick={() => {
+									cartContext?.removeItemFromCart(li.id);
+								}}
+							/>
+							<p>{li.quantity}</p>
+							<AddIcon
+								onClick={() => {
+									cartContext?.addItemToCart({
+										id: li.id,
+										name: li.name,
+										price: li.price,
+										quantity: 1,
+									});
+								}}
+							/>
+						</div>
 					</div>
 				);
 			})}
